286775039fe3a2fd2629a08df494d14f10891d11
Hamidreza Behzadi <hamidbehzadi71@gmail.com>
2025-10-25 11:50:42 +0330

    feat: add tailwindcss - husky - prettier - lintStage

---
+++ .prettierrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  singleQuote: true,
+  bracketSameLine: true,
+  trailingComma: 'es5',
+  printWidth: 80,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
