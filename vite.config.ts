daedd30039e74596e2159959f9869192ac402926
Hamidreza Behzadi <hamidbehzadi71@gmail.com>
2025-10-26 14:23:05 +0330

    fix: workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,19 @@ import tailwindcss from '@tailwindcss/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss()],
-  base: process.env.NODE_ENV === 'development' ? '/' : '/omnirev/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api-mock.omnirev.ai',
-        changeOrigin: true,
-        rewrite: path => path.replace(/^\/api/, ''),
-        secure: false,
+export default defineConfig(({mode}) => {
+  return {
+    plugins: [react(), tsconfigPaths(), tailwindcss()],
+    base: mode === 'development' ? '/' : '/omnirev/',
+    server: {
+      proxy: {
+        '/api': {
+          target: 'https://api-mock.omnirev.ai',
+          changeOrigin: true,
+          rewrite: path => path.replace(/^\/api/, ''),
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
